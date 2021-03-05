7e4a35819d0cf7d88e0be430e30627f2b516ff3a
Farid Bettioui <bettioui.farid272@gmail.com>
2021-03-05 15:08:49 +0100

    add some retouch

--- sha1.py
+++ sha1.py
@@ -5,15 +5,15 @@ def shaone (text):
 
     # encode the string
     encoded_str = text.encode()
-    
+
     # create a sha1 hash object initialized with the encoded string
     hash_obj = hashlib.sha1(encoded_str)
-    
+
     # convert the hash object to a hexadecimal value
     hexa_value = hash_obj.hexdigest()
 
     # print
-    print("\n", hexa_value, "\n")
+    #print("\n h = ", hexa_value)
 
     return hexa_value
 
@@ -22,13 +22,11 @@ def H(text1,text2):
     y = text2
     x = 0
 
-    while x < 100 :
-        h = shaone(id + str(x) )
-        
+    while True :
+        print('\n\n ==> X = ',x ,'\n ===> X apres le hashage est : ',shaone( str(x) ))
+        h = shaone( id + str(x) )
         if h <= y :
-            print('le nombre de tentative est: ',x)
-            print( shaone(x) )
-
+            print('====> la valeur de H apres le hashage : ',shaone(id + str(x)) )
             return x
         
         x += 1
@@ -39,4 +37,3 @@ id = "bettiouifarid"
 y = "03b1663dda6549a0939ffdd712a852e0d4234e6b"
 
 x = H(id , y)
-
