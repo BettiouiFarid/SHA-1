a7df8aa4ae963b993dcf2842dc9e2a8ca3918515
Farid Bettioui <bettioui.farid272@gmail.com>
2021-03-05 21:53:17 +0100

    add SHA256

--- sha256.py
+++ sha256.py
@@ -1,12 +1,12 @@
 # import the library module
 import hashlib
 
-def shaone (text):
+def sha256 (text):
     # encode the string
     encoded_str = text.encode()
 
     # create a sha1 hash object initialized with the encoded string
-    hash_obj = hashlib.sha1(encoded_str)
+    hash_obj = hashlib.sha256(encoded_str)
 
     # convert the hash object to a hexadecimal value
     hexa_value = hash_obj.hexdigest()
@@ -16,20 +16,20 @@ def shaone (text):
 def H(text1,text2):
     id = text1
     y = text2
-    x = 0   #35
+    x = 0      #10000000015880         
 
     while True :
         
-        print('\n\n==> X = ',x ,'\n===> X apres le hashage est : ',shaone( str(x) ))
-        h = shaone( id + str(x) )
+        print('\n\n==> X = ',x ,'\n===> X apres le hashage est : ',sha256( str(x) ))
+        h = sha256( id + str(x) )
         
         if h <= y :
-            print('====> la valeur de H apres le hashage : ',shaone(id + str(x)) )
+            print('====> la valeur de H apres le hashage : ',sha256(id + str(x)) )
             return x
         
         x += 1
         
 id = "bettiouifarid"
-y = "03b1663dda6549a0939ffdd712a852e0d4234e6b"
+y = "00005d10cc11e27bd8d4d1ce91bc725665ddbaa6ca2498ef38a88a58ad48cdb4"
 
 x = H(id , y)
